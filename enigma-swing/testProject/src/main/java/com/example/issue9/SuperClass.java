681b7b3d0b6fd0e3d7398382089b1262524bcf27
ByMartrixx <bymartrixx@gmail.com>
2022-03-02 14:16:04 -0300

    Rename issue reproduction package to issue<Number>

--- enigma-swing/testProject/src/main/java/com/example/issue9/SuperClass.java
+++ enigma-swing/testProject/src/main/java/com/example/issue9/SuperClass.java
@@ -1,4 +1,9 @@
-package com.example.bridgemethodsissue;
+/*
+ * Class to reproduce issue #9
+ * https://github.com/QuiltMC/enigma/issues/9
+ */
+
+package com.example.issue9;
 
 public class SuperClass {
     private int x;
