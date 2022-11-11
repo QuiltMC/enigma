74abe5272e7d4b8cdbb23a1752036ba24b9363ad
kadir1243 <75079840+kadir1243@users.noreply.github.com>
2022-11-11 17:29:51 +0300

    add back jimfs

--- enigma/src/test/java/cuchaz/enigma/translation/mapping/serde/recaf/TestRecaf.java
+++ enigma/src/test/java/cuchaz/enigma/translation/mapping/serde/recaf/TestRecaf.java
@@ -1,14 +1,19 @@
 package cuchaz.enigma.translation.mapping.serde.recaf;
 
+import com.google.common.jimfs.Jimfs;
 import cuchaz.enigma.ProgressListener;
 import cuchaz.enigma.translation.mapping.EntryMapping;
 import cuchaz.enigma.translation.mapping.tree.EntryTree;
 import org.junit.jupiter.api.Test;
 
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,20 +21,25 @@ public class TestRecaf {
 
     @Test
     public void testIntegrity() throws Exception {
-        Path path = Path.of("src/test/resources/recaf.mappings");
-        Set<String> contents = new HashSet<>(Files.readAllLines(path));
-        Path tempFile = Files.createTempFile(null, null);
-        Files.write(tempFile, contents);
+        Set<String> contents;
+        try (InputStream in = getClass().getResourceAsStream("/recaf.mappings")) {
+            contents = new String(in.readAllBytes(), StandardCharsets.UTF_8).lines().collect(Collectors.toSet());
+        }
 
-        RecafMappingsWriter writer = RecafMappingsWriter.INSTANCE;
-        RecafMappingsReader reader = RecafMappingsReader.INSTANCE;
+        try (FileSystem fs = Jimfs.newFileSystem()) {
+            Path path = fs.getPath("recaf.mappings");
+            Files.write(path, contents);
 
-        EntryTree<EntryMapping> mappings = reader.read(tempFile, ProgressListener.none(), null);
-        writer.write(mappings, tempFile, ProgressListener.none(), null);
+            RecafMappingsWriter writer = RecafMappingsWriter.INSTANCE;
+            RecafMappingsReader reader = RecafMappingsReader.INSTANCE;
 
-        reader.read(tempFile, ProgressListener.none(), null);
-        Set<String> newContents = new HashSet<>(Files.readAllLines(tempFile));
+            EntryTree<EntryMapping> mappings = reader.read(path, ProgressListener.none(), null);
+            writer.write(mappings, path, ProgressListener.none(), null);
 
-        assertEquals(contents, newContents);
+            reader.read(path, ProgressListener.none(), null);
+            Set<String> newContents = new HashSet<>(Files.readAllLines(path));
+
+            assertEquals(contents, newContents);
+        }
     }
 }
