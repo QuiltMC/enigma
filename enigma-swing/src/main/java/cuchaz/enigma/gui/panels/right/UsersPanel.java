53e34ae48fcfe1305fb993e5c1aa38f7051a7a04
ix0rai <ix0rai64@gmail.com>
2022-12-29 21:47:33 -0600

    better consistency in UsersPanel

--- enigma-swing/src/main/java/cuchaz/enigma/gui/panels/right/UsersPanel.java
+++ enigma-swing/src/main/java/cuchaz/enigma/gui/panels/right/UsersPanel.java
@@ -2,14 +2,20 @@ package cuchaz.enigma.gui.panels.right;
 
 import cuchaz.enigma.gui.Gui;
 
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import java.awt.BorderLayout;
 
 public class UsersPanel extends MultiplayerOnlyRightPanel {
-    private final JScrollPane userScrollPane;
+	private final JPanel topPanel;
 
     public UsersPanel(Gui gui) {
         super(gui);
-        this.userScrollPane = new JScrollPane(gui.getUsers());
+		JScrollPane userScrollPane = new JScrollPane(gui.getUsers());
+		this.topPanel = new JPanel(new BorderLayout());
+
+		this.topPanel.add(this.title, BorderLayout.NORTH);
+		this.topPanel.add(userScrollPane, BorderLayout.SOUTH);
 
         // set state
         this.setUp(!gui.isOffline());
@@ -18,7 +24,7 @@ public class UsersPanel extends MultiplayerOnlyRightPanel {
     @Override
 	protected void addComponents() {
 		super.addComponents();
-        this.add(userScrollPane);
+        this.add(topPanel, BorderLayout.NORTH);
     }
 
     @Override
