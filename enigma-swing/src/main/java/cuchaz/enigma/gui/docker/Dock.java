073a2fa46ae5851a41e328cca66cdc10ddc72a23
ix0rai <ix0rai64@gmail.com>
2023-01-18 19:26:30 -0600

    decrease code duplication a little

--- enigma-swing/src/main/java/cuchaz/enigma/gui/docker/Dock.java
+++ enigma-swing/src/main/java/cuchaz/enigma/gui/docker/Dock.java
@@ -389,12 +389,11 @@ public class Dock extends JPanel {
 			for (Dock dock : INSTANCES) {
 				for (Docker d : dock.getDockers()) {
 					if (d != null && d.getId().equals(docker.getId())) {
-						if (dock.unifiedDock != null && d.equals(dock.unifiedDock.getHostedDocker())) {
-							return new Docker.Location(dock.side, Docker.VerticalLocation.FULL);
-						} else if (d.equals(dock.topDock.getHostedDocker())) {
-							return new Docker.Location(dock.side, Docker.VerticalLocation.TOP);
-						} else if (d.equals(dock.bottomDock.getHostedDocker())) {
-							return new Docker.Location(dock.side, Docker.VerticalLocation.BOTTOM);
+						// note: we declare our array ourselves so that we can control the order
+						for (Docker.VerticalLocation verticalLocation : new Docker.VerticalLocation[]{Docker.VerticalLocation.FULL, Docker.VerticalLocation.TOP, Docker.VerticalLocation.BOTTOM}) {
+							if (dock.getDock(verticalLocation).getHostedDocker().equals(d)) {
+								return new Docker.Location(dock.side, verticalLocation);
+							}
 						}
 					}
 				}
