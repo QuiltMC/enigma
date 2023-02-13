b1117e76f35999f45e06e8eb882f9ba0c209979c
ix0rai <ix0rai64@gmail.com>
2023-02-12 20:02:37 -0600

    perfect visuals on new buttons

--- enigma-swing/src/main/java/cuchaz/enigma/gui/docker/component/DockerButton.java
+++ enigma-swing/src/main/java/cuchaz/enigma/gui/docker/component/DockerButton.java
@@ -1,26 +1,28 @@
 package cuchaz.enigma.gui.docker.component;
 
 import cuchaz.enigma.gui.config.UiConfig;
+import cuchaz.enigma.gui.docker.Docker;
 
-import javax.swing.Icon;
-import javax.swing.JLabel;
 import javax.swing.JToggleButton;
-import javax.swing.SwingUtilities;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Insets;
-import java.awt.Rectangle;
 import java.awt.RenderingHints;
 
 public class DockerButton extends JToggleButton {
 	private String text;
+	private Docker.Side side;
 
-	public DockerButton(String text) {
-		// todo rotation
+	public DockerButton(String text, Docker.Side side) {
 		super("");
 		this.text = text;
+		this.side = side;
+	}
+
+	public void setSide(Docker.Side side) {
+		this.side = side;
+		this.repaint();
 	}
 
 	@Override
@@ -34,11 +36,21 @@ public class DockerButton extends JToggleButton {
 
 		Graphics2D g2d = (Graphics2D) g;
 
-		// todo better padding solution
-		g2d.rotate(-3 * (Math.PI * 0.5));
+		// rotate
+		g2d.rotate(-(this.side == Docker.Side.RIGHT ? 3 : 1) * (Math.PI * 0.5));
+
+		// setup text
 		Font font = UiConfig.getDefault2Font();
-		this.setPreferredSize(new Dimension(this.getPreferredSize().width, (int) font.createGlyphVector(g2d.getFontRenderContext(), text).getVisualBounds().getWidth() + 25));
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		g2d.drawString(this.text, 10, -10);
+		g2d.setFont(font);
+
+		// position
+		int textSize = (int) font.createGlyphVector(g2d.getFontRenderContext(), text).getVisualBounds().getWidth() + 20;
+		this.setPreferredSize(new Dimension(this.getPreferredSize().width, textSize));
+		int x = this.side == Docker.Side.RIGHT ? 10 : -textSize + 10;
+		int y = this.side == Docker.Side.RIGHT ? -10 : 20;
+
+		g2d.drawString(this.text, x, y);
+		this.setSize(this.getPreferredSize());
 	}
 }
