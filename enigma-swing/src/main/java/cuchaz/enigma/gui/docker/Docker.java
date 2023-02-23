b120ad1576409e0a82c58eec956c2052f3068b82
ix0rai <ix0rai64@gmail.com>
2023-02-23 16:36:36 -0600

    automatically retranslate buttons

--- enigma-swing/src/main/java/cuchaz/enigma/gui/docker/Docker.java
+++ enigma-swing/src/main/java/cuchaz/enigma/gui/docker/Docker.java
@@ -31,7 +31,7 @@ public abstract class Docker extends JPanel {
 		super(new BorderLayout());
 		this.gui = gui;
 		this.title = new DockerTitleBar(this, this.titleSupplier);
-		this.button = new DockerButton(this, this.titleSupplier.get(), this.getButtonLocation().side);
+		this.button = new DockerButton(this, this.titleSupplier, this.getButtonLocation().side);
 		// add action listener to open and close the docker when its button is pressed
 		this.button.addActionListener(e -> {
 			Docker docker = getDocker(this.getClass());
@@ -52,7 +52,6 @@ public abstract class Docker extends JPanel {
 	}
 
 	public void retranslateUi() {
-		//this.button.setText(this.titleSupplier.get());
 		this.title.retranslateUi();
 	}
 
--- enigma-swing/src/main/java/cuchaz/enigma/gui/docker/component/DockerButton.java
+++ enigma-swing/src/main/java/cuchaz/enigma/gui/docker/component/DockerButton.java
@@ -12,6 +12,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.event.MouseEvent;
+import java.util.function.Supplier;
 
 public class DockerButton extends JToggleButton implements Draggable {
 	private final Docker docker;
@@ -20,13 +21,13 @@ public class DockerButton extends JToggleButton implements Draggable {
 	private Object constraints;
 	private boolean cancelEvents;
 	private boolean mousePressed;
-	private String text;
+	private final Supplier<String> textSupplier;
 	private Docker.Side side;
 
-	public DockerButton(Docker docker, String text, Docker.Side side) {
+	public DockerButton(Docker docker, Supplier<String> textSupplier, Docker.Side side) {
 		super("");
 		this.docker = docker;
-		this.text = text;
+		this.textSupplier = textSupplier;
 		this.side = side;
 
 		this.addMouseListener(this.getMouseListener());
@@ -104,7 +105,7 @@ public class DockerButton extends JToggleButton implements Draggable {
 
 	@Override
 	public void setText(String text) {
-		this.text = text;
+		throw new UnsupportedOperationException("cannot set text on a docker button! you should be setting the text supplier to provide translated text instead.");
 	}
 
 	@Override
@@ -117,6 +118,7 @@ public class DockerButton extends JToggleButton implements Draggable {
 		g2d.rotate(-(this.side == Docker.Side.RIGHT ? 3 : 1) * (Math.PI * 0.5));
 
 		// setup text
+		String translatedText = this.textSupplier.get();
 		Font font = UiConfig.getDefault2Font();
 		if (UiConfig.getLookAndFeel().equals(LookAndFeel.SYSTEM)) {
 			font = font.deriveFont(Font.BOLD);
@@ -125,12 +127,12 @@ public class DockerButton extends JToggleButton implements Draggable {
 		g2d.setFont(font);
 
 		// position
-		int textSize = (int) font.createGlyphVector(g2d.getFontRenderContext(), text).getVisualBounds().getWidth() + 20;
+		int textSize = (int) font.createGlyphVector(g2d.getFontRenderContext(), translatedText).getVisualBounds().getWidth() + 20;
 		this.setPreferredSize(new Dimension(this.getPreferredSize().width, textSize));
 		int x = this.side == Docker.Side.RIGHT ? 10 : -textSize + 10;
 		int y = this.side == Docker.Side.RIGHT ? -10 : 20;
 
-		g2d.drawString(this.text, x, y);
+		g2d.drawString(translatedText, x, y);
 		this.setSize(this.getPreferredSize());
 	}
 }
