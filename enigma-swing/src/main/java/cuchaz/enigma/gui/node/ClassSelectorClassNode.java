780be66050e478ef1686fbe1b7a2d4685bd42b50
ix0rai <ix0rai64@gmail.com>
2023-04-10 19:35:00 -0500

    Revert "reload parent of updated class node upon stat reload completion (fixes #89)"
    This reverts commit 0f85045474c938735efd8e85b8036448433ff0ea.

--- enigma-swing/src/main/java/cuchaz/enigma/gui/node/ClassSelectorClassNode.java
+++ enigma-swing/src/main/java/cuchaz/enigma/gui/node/ClassSelectorClassNode.java
@@ -61,7 +61,7 @@ public class ClassSelectorClassNode extends DefaultMutableTreeNode {
 			@Override
 			public void done() {
 				((DefaultTreeCellRenderer) selector.getCellRenderer()).setIcon(GuiUtil.getDeobfuscationIcon(ClassSelectorClassNode.this.getStats()));
-				selector.reload(ClassSelectorClassNode.this.getParent());
+				selector.reload(ClassSelectorClassNode.this);
 			}
 		};
 
