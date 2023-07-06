38abdd9c4088efae718d8fd450a7a9559147717f
ix0rai <ix0rai64@gmail.com>
2023-07-05 21:41:24 -0500

    parameter validator

--- enigma/src/main/java/cuchaz/enigma/translation/mapping/MappingValidator.java
+++ enigma/src/main/java/cuchaz/enigma/translation/mapping/MappingValidator.java
@@ -2,8 +2,13 @@ package cuchaz.enigma.translation.mapping;
 
 import cuchaz.enigma.analysis.index.JarIndex;
 import cuchaz.enigma.translation.Translator;
+import cuchaz.enigma.translation.representation.AccessFlags;
+import cuchaz.enigma.translation.representation.ArgumentDescriptor;
+import cuchaz.enigma.translation.representation.MethodDescriptor;
 import cuchaz.enigma.translation.representation.entry.ClassEntry;
 import cuchaz.enigma.translation.representation.entry.Entry;
+import cuchaz.enigma.translation.representation.entry.LocalVariableEntry;
+import cuchaz.enigma.translation.representation.entry.MethodEntry;
 import cuchaz.enigma.translation.representation.entry.ParentedEntry;
 import cuchaz.enigma.utils.validation.Message;
 import cuchaz.enigma.utils.validation.ValidationContext;
@@ -39,9 +44,12 @@ public class MappingValidator {
 	private boolean validateUnique(ValidationContext context, Entry<?> entry, String name) {
 		ClassEntry containingClass = entry.getContainingClass();
 		Entry<?> translatedEntry = this.deobfuscator.translate(entry);
-		List<ParentedEntry<?>> siblings = this.index.getChildrenByClass().get(containingClass);
 
-		// todo: broken on parameters
+		if (entry instanceof LocalVariableEntry parameter) {
+			return this.validateParameterUniqueness(context, name, parameter);
+		}
+
+		List<ParentedEntry<?>> siblings = this.index.getChildrenByClass().get(containingClass);
 
 		// add all ancestors
 		for (ClassEntry ancestor : this.index.getInheritanceIndex().getAncestors(containingClass)) {
@@ -58,6 +66,32 @@ public class MappingValidator {
 		if (translatedEntry != null && !this.isUnique(translatedEntry, siblings, name)) {
 			this.raiseConflict(context, translatedEntry.getParent(), name);
 			return true;
+		}
+
+		return false;
+	}
+
+	private boolean validateParameterUniqueness(ValidationContext context, String name, LocalVariableEntry parameter) {
+		MethodEntry parent = parameter.getParent();
+		if (parent != null) {
+			MethodDescriptor desc = parent.getDesc();
+			AccessFlags flags = this.index.getEntryIndex().getMethodAccess(parent);
+
+			if (desc != null && flags != null) {
+				int argIndex = flags.isStatic() ? 0 : 1;
+
+				for (ArgumentDescriptor arg : desc.getArgumentDescs()) {
+					LocalVariableEntry argEntry = new LocalVariableEntry(parent, argIndex, "", true, null);
+					LocalVariableEntry translatedArgEntry = this.deobfuscator.translate(argEntry);
+
+					if (translatedArgEntry != null && translatedArgEntry.getName().equals(name)) {
+						this.raiseConflict(context, parent, name);
+						return true;
+					}
+
+					argIndex += arg.getSize();
+				}
+			}
 		}
 
 		return false;
