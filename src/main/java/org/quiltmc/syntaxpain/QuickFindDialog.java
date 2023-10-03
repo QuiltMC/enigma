9b8ee1b8e0611e9b7bed32618b5d1f4a3568bf62
ix0rai <ix0rai64@gmail.com>
2023-10-02 22:07:50 -0500

    fix checkstyle

--- src/main/java/org/quiltmc/syntaxpain/QuickFindDialog.java
+++ src/main/java/org/quiltmc/syntaxpain/QuickFindDialog.java
@@ -34,238 +34,238 @@ import java.util.regex.PatternSyntaxException;
  * Designed to be extensible.
  */
 public class QuickFindDialog extends JDialog implements DocumentListener, ActionListener, EscapeListener {
-    protected static final int SEARCH_FIELD_MAX_WIDTH = 200;
-    protected static final int SEARCH_FIELD_MAX_HEIGHT = 24;
-    protected static final int SEARCH_FIELD_MIN_WIDTH = 60;
-    private static final int SEARCH_FIELD_MIN_HEIGHT = 24;
-    protected static final int PREFERRED_TOOLBAR_WIDTH = 684;
+	protected static final int SEARCH_FIELD_MAX_WIDTH = 200;
+	protected static final int SEARCH_FIELD_MAX_HEIGHT = 24;
+	protected static final int SEARCH_FIELD_MIN_WIDTH = 60;
+	private static final int SEARCH_FIELD_MIN_HEIGHT = 24;
+	protected static final int PREFERRED_TOOLBAR_WIDTH = 684;
 
-    protected final Markers.SimpleMarker marker = new Markers.SimpleMarker(Color.PINK);
-    protected WeakReference<JTextComponent> target;
-    protected final WeakReference<DocumentSearchData> searchData;
-    protected int prevCaretPos;
+	protected final Markers.SimpleMarker marker = new Markers.SimpleMarker(Color.PINK);
+	protected WeakReference<JTextComponent> target;
+	protected final WeakReference<DocumentSearchData> searchData;
+	protected int prevCaretPos;
 
-    protected JLabel statusLabel;
-    protected JTextField searchField;
-    protected JButton prevButton;
-    protected JButton nextButton;
-    protected JCheckBox ignoreCaseCheckBox;
-    protected JCheckBox regexCheckBox;
-    protected JCheckBox wrapCheckBox;
+	protected JLabel statusLabel;
+	protected JTextField searchField;
+	protected JButton prevButton;
+	protected JButton nextButton;
+	protected JCheckBox ignoreCaseCheckBox;
+	protected JCheckBox regexCheckBox;
+	protected JCheckBox wrapCheckBox;
 
-    public QuickFindDialog(JTextComponent target) {
-        this(target, DocumentSearchData.getFromEditor(target));
-    }
+	public QuickFindDialog(JTextComponent target) {
+		this(target, DocumentSearchData.getFromEditor(target));
+	}
 
-    public QuickFindDialog(JTextComponent target, DocumentSearchData searchData) {
-        super(SwingUtilities.getWindowAncestor(target), ModalityType.MODELESS);
+	public QuickFindDialog(JTextComponent target, DocumentSearchData searchData) {
+		super(SwingUtilities.getWindowAncestor(target), ModalityType.MODELESS);
 
-        this.initComponents();
-        Util.addEscapeListener(this);
-        this.searchData = new WeakReference<>(searchData);
-    }
+		this.initComponents();
+		Util.addEscapeListener(this);
+		this.searchData = new WeakReference<>(searchData);
+	}
 
-    public void showFor(JTextComponent target) {
-        this.prevCaretPos = target.getCaretPosition();
+	public void showFor(JTextComponent target) {
+		this.prevCaretPos = target.getCaretPosition();
 
-        Container view = target.getParent();
-        Dimension size = this.getSize();
+		Container view = target.getParent();
+		Dimension size = this.getSize();
 
-        // Set the width of the dialog to the width of the target
-        size.width = target.getVisibleRect().width;
-        this.setSize(size);
+		// Set the width of the dialog to the width of the target
+		size.width = target.getVisibleRect().width;
+		this.setSize(size);
 
-        // Put the dialog at the bottom of the target
-        Point loc = new Point(0, view.getHeight() - size.height);
-        this.setLocationRelativeTo(view);
-        SwingUtilities.convertPointToScreen(loc, view);
-        this.setLocation(loc);
+		// Put the dialog at the bottom of the target
+		Point loc = new Point(0, view.getHeight() - size.height);
+		this.setLocationRelativeTo(view);
+		SwingUtilities.convertPointToScreen(loc, view);
+		this.setLocation(loc);
 
-        this.searchField.setFont(target.getFont());
-        this.searchField.getDocument().addDocumentListener(this);
+		this.searchField.setFont(target.getFont());
+		this.searchField.getDocument().addDocumentListener(this);
 
-        // Close the dialog when clicking outside it
-        WindowFocusListener focusListener = new WindowAdapter() {
-            @Override
-            public void windowLostFocus(WindowEvent e) {
-                target.getDocument().removeDocumentListener(QuickFindDialog.this);
-                Markers.removeMarkers(target, QuickFindDialog.this.marker);
-                QuickFindDialog.this.removeWindowListener(this);
-                QuickFindDialog.this.setVisible(false);
-                target.requestFocus();
-            }
-        };
-        this.addWindowFocusListener(focusListener);
+		// Close the dialog when clicking outside it
+		WindowFocusListener focusListener = new WindowAdapter() {
+			@Override
+			public void windowLostFocus(WindowEvent e) {
+				target.getDocument().removeDocumentListener(QuickFindDialog.this);
+				Markers.removeMarkers(target, QuickFindDialog.this.marker);
+				QuickFindDialog.this.removeWindowListener(this);
+				QuickFindDialog.this.setVisible(false);
+				target.requestFocus();
+			}
+		};
+		this.addWindowFocusListener(focusListener);
 
-        this.target = new WeakReference<>(target);
+		this.target = new WeakReference<>(target);
 
-        DocumentSearchData searchData = this.searchData.get();
-        this.wrapCheckBox.setSelected(searchData.isWrap());
+		DocumentSearchData searchData = this.searchData.get();
+		this.wrapCheckBox.setSelected(searchData.isWrap());
 
-        // Set the search field to the current selection
-        String selectedText = target.getSelectedText();
-        if (selectedText != null) {
-            this.searchField.setText(selectedText);
-        } else {
-            Pattern pattern = searchData.getPattern();
-            if (pattern != null) {
-                this.searchField.setText(pattern.pattern());
-            }
-        }
+		// Set the search field to the current selection
+		String selectedText = target.getSelectedText();
+		if (selectedText != null) {
+			this.searchField.setText(selectedText);
+		} else {
+			Pattern pattern = searchData.getPattern();
+			if (pattern != null) {
+				this.searchField.setText(pattern.pattern());
+			}
+		}
 
-        this.searchField.selectAll();
+		this.searchField.selectAll();
 
-        this.setVisible(true);
-    }
+		this.setVisible(true);
+	}
 
-    protected void initComponents() {
-        JToolBar toolBar = new JToolBar();
-        this.statusLabel = new JLabel();
-        this.searchField = new JTextField();
-        this.prevButton = new JButton("prev");
-        this.nextButton = new JButton("next");
-        this.ignoreCaseCheckBox = new JCheckBox();
-        this.regexCheckBox = new JCheckBox();
-        this.wrapCheckBox = new JCheckBox();
+	protected void initComponents() {
+		JToolBar toolBar = new JToolBar();
+		this.statusLabel = new JLabel();
+		this.searchField = new JTextField();
+		this.prevButton = new JButton("prev");
+		this.nextButton = new JButton("next");
+		this.ignoreCaseCheckBox = new JCheckBox();
+		this.regexCheckBox = new JCheckBox();
+		this.wrapCheckBox = new JCheckBox();
 
-        this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        this.setBackground(Color.DARK_GRAY);
-        this.setName("QuickFindDialog");
-        this.setResizable(false);
-        this.setUndecorated(true);
+		this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		this.setBackground(Color.DARK_GRAY);
+		this.setName("QuickFindDialog");
+		this.setResizable(false);
+		this.setUndecorated(true);
 
-        toolBar.setBorder(BorderFactory.createEtchedBorder());
-        toolBar.setFloatable(false);
-        toolBar.setRollover(true);
-        toolBar.addSeparator();
+		toolBar.setBorder(BorderFactory.createEtchedBorder());
+		toolBar.setFloatable(false);
+		toolBar.setRollover(true);
+		toolBar.addSeparator();
 
-        this.searchField.setColumns(30);
-        this.searchField.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        this.searchField.setMaximumSize(new Dimension(SEARCH_FIELD_MAX_WIDTH, SEARCH_FIELD_MAX_HEIGHT));
-        this.searchField.setMinimumSize(new Dimension(SEARCH_FIELD_MIN_WIDTH, SEARCH_FIELD_MIN_HEIGHT));
-        toolBar.add(this.searchField);
-        toolBar.addSeparator();
+		this.searchField.setColumns(30);
+		this.searchField.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		this.searchField.setMaximumSize(new Dimension(SEARCH_FIELD_MAX_WIDTH, SEARCH_FIELD_MAX_HEIGHT));
+		this.searchField.setMinimumSize(new Dimension(SEARCH_FIELD_MIN_WIDTH, SEARCH_FIELD_MIN_HEIGHT));
+		toolBar.add(this.searchField);
+		toolBar.addSeparator();
 
-        this.prevButton.setHorizontalTextPosition(SwingConstants.CENTER);
-        this.prevButton.setFocusable(false);
-        this.prevButton.setOpaque(false);
-        this.prevButton.setVerticalTextPosition(SwingConstants.BOTTOM);
-        this.prevButton.addActionListener(this::prevButtonActionPerformed);
-        toolBar.add(this.prevButton);
+		this.prevButton.setHorizontalTextPosition(SwingConstants.CENTER);
+		this.prevButton.setFocusable(false);
+		this.prevButton.setOpaque(false);
+		this.prevButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+		this.prevButton.addActionListener(this::prevButtonActionPerformed);
+		toolBar.add(this.prevButton);
 
-        this.nextButton.setHorizontalTextPosition(SwingConstants.CENTER);
-        this.nextButton.setMargin(new Insets(2, 2, 2, 2));
-        this.nextButton.setFocusable(false);
-        this.nextButton.setOpaque(false);
-        this.nextButton.setVerticalTextPosition(SwingConstants.BOTTOM);
-        this.nextButton.addActionListener(this::nextButtonActionPerformed);
-        toolBar.add(this.nextButton);
+		this.nextButton.setHorizontalTextPosition(SwingConstants.CENTER);
+		this.nextButton.setMargin(new Insets(2, 2, 2, 2));
+		this.nextButton.setFocusable(false);
+		this.nextButton.setOpaque(false);
+		this.nextButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+		this.nextButton.addActionListener(this::nextButtonActionPerformed);
+		toolBar.add(this.nextButton);
 
-        toolBar.addSeparator();
+		toolBar.addSeparator();
 
-        this.ignoreCaseCheckBox.setText("Ignore case");
-        this.ignoreCaseCheckBox.setFocusable(false);
-        this.ignoreCaseCheckBox.setOpaque(false);
-        this.ignoreCaseCheckBox.setVerticalTextPosition(SwingConstants.BOTTOM);
-        this.ignoreCaseCheckBox.addActionListener(this);
-        toolBar.add(this.ignoreCaseCheckBox);
+		this.ignoreCaseCheckBox.setText("Ignore case");
+		this.ignoreCaseCheckBox.setFocusable(false);
+		this.ignoreCaseCheckBox.setOpaque(false);
+		this.ignoreCaseCheckBox.setVerticalTextPosition(SwingConstants.BOTTOM);
+		this.ignoreCaseCheckBox.addActionListener(this);
+		toolBar.add(this.ignoreCaseCheckBox);
 
-        this.regexCheckBox.setText("Use regex");
-        this.regexCheckBox.setFocusable(false);
-        this.regexCheckBox.setOpaque(false);
-        this.regexCheckBox.setVerticalTextPosition(SwingConstants.BOTTOM);
-        this.regexCheckBox.addActionListener(this);
-        toolBar.add(this.regexCheckBox);
+		this.regexCheckBox.setText("Use regex");
+		this.regexCheckBox.setFocusable(false);
+		this.regexCheckBox.setOpaque(false);
+		this.regexCheckBox.setVerticalTextPosition(SwingConstants.BOTTOM);
+		this.regexCheckBox.addActionListener(this);
+		toolBar.add(this.regexCheckBox);
 
-        this.wrapCheckBox.setText("Wrap");
-        this.wrapCheckBox.setFocusable(false);
-        this.wrapCheckBox.setOpaque(false);
-        this.wrapCheckBox.setVerticalTextPosition(SwingConstants.BOTTOM);
-        this.wrapCheckBox.addActionListener(this);
-        toolBar.add(this.wrapCheckBox);
+		this.wrapCheckBox.setText("Wrap");
+		this.wrapCheckBox.setFocusable(false);
+		this.wrapCheckBox.setOpaque(false);
+		this.wrapCheckBox.setVerticalTextPosition(SwingConstants.BOTTOM);
+		this.wrapCheckBox.addActionListener(this);
+		toolBar.add(this.wrapCheckBox);
 
-        this.statusLabel.setFont(this.statusLabel.getFont().deriveFont(this.statusLabel.getFont().getStyle() | Font.BOLD, this.statusLabel.getFont().getSize() - 2));
-        this.statusLabel.setForeground(Color.RED);
-        toolBar.add(this.statusLabel);
+		this.statusLabel.setFont(this.statusLabel.getFont().deriveFont(this.statusLabel.getFont().getStyle() | Font.BOLD, this.statusLabel.getFont().getSize() - 2));
+		this.statusLabel.setForeground(Color.RED);
+		toolBar.add(this.statusLabel);
 
-        GroupLayout layout = new GroupLayout(this.getContentPane());
-        this.getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                .addComponent(toolBar, GroupLayout.DEFAULT_SIZE, PREFERRED_TOOLBAR_WIDTH, Short.MAX_VALUE));
-        layout.setVerticalGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                .addComponent(toolBar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE));
+		GroupLayout layout = new GroupLayout(this.getContentPane());
+		this.getContentPane().setLayout(layout);
+		layout.setHorizontalGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+				.addComponent(toolBar, GroupLayout.DEFAULT_SIZE, PREFERRED_TOOLBAR_WIDTH, Short.MAX_VALUE));
+		layout.setVerticalGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+				.addComponent(toolBar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE));
 
-        this.pack();
-    }
+		this.pack();
+	}
 
-    private void prevButtonActionPerformed(ActionEvent e) {
-        if (this.searchData.get().doFindPrev(this.target.get())) {
-            this.statusLabel.setText(null);
-        } else {
-            this.statusLabel.setText("QuickFindDialog.NotFound");
-        }
-    }
+	private void prevButtonActionPerformed(ActionEvent e) {
+		if (this.searchData.get().doFindPrev(this.target.get())) {
+			this.statusLabel.setText(null);
+		} else {
+			this.statusLabel.setText("QuickFindDialog.NotFound");
+		}
+	}
 
-    private void nextButtonActionPerformed(ActionEvent e) {
-        if (this.searchData.get().doFindNext(this.target.get())) {
-            this.statusLabel.setText(null);
-        } else {
-            this.statusLabel.setText("QuickFindDialog.NotFound");
-        }
-    }
+	private void nextButtonActionPerformed(ActionEvent e) {
+		if (this.searchData.get().doFindNext(this.target.get())) {
+			this.statusLabel.setText(null);
+		} else {
+			this.statusLabel.setText("QuickFindDialog.NotFound");
+		}
+	}
 
-    @Override
-    public void insertUpdate(DocumentEvent e) {
-        this.updateFind();
-    }
+	@Override
+	public void insertUpdate(DocumentEvent e) {
+		this.updateFind();
+	}
 
-    @Override
-    public void removeUpdate(DocumentEvent e) {
-        this.updateFind();
-    }
+	@Override
+	public void removeUpdate(DocumentEvent e) {
+		this.updateFind();
+	}
 
-    @Override
-    public void changedUpdate(DocumentEvent e) {
-        this.updateFind();
-    }
+	@Override
+	public void changedUpdate(DocumentEvent e) {
+		this.updateFind();
+	}
 
-    private void updateFind() {
-        JTextComponent target = this.target.get();
-        DocumentSearchData searchData = this.searchData.get();
-        String searchText = this.searchField.getText();
+	private void updateFind() {
+		JTextComponent target = this.target.get();
+		DocumentSearchData searchData = this.searchData.get();
+		String searchText = this.searchField.getText();
 
-        if (searchText == null || searchText.isEmpty()) {
-            this.statusLabel.setText(null);
-            return;
-        }
+		if (searchText == null || searchText.isEmpty()) {
+			this.statusLabel.setText(null);
+			return;
+		}
 
-        try {
-            searchData.setWrap(this.wrapCheckBox.isSelected());
-            searchData.setPattern(searchText, this.regexCheckBox.isSelected(), this.ignoreCaseCheckBox.isSelected());
-            this.statusLabel.setText(null);
+		try {
+			searchData.setWrap(this.wrapCheckBox.isSelected());
+			searchData.setPattern(searchText, this.regexCheckBox.isSelected(), this.ignoreCaseCheckBox.isSelected());
+			this.statusLabel.setText(null);
 
-            // The DocumentSearchData doFindNext will always find from current pos,
-            // so we need to relocate to our saved pos before we call doFindNext
-            target.setCaretPosition(this.prevCaretPos);
-            if (!searchData.doFindNext(target)) {
-                this.statusLabel.setText("QuickFindDialog.NotFound");
-            } else {
-                this.statusLabel.setText(null);
-            }
-        } catch (PatternSyntaxException e) {
-            this.statusLabel.setText(e.getDescription());
-        }
-    }
+			// The DocumentSearchData doFindNext will always find from current pos,
+			// so we need to relocate to our saved pos before we call doFindNext
+			target.setCaretPosition(this.prevCaretPos);
+			if (!searchData.doFindNext(target)) {
+				this.statusLabel.setText("QuickFindDialog.NotFound");
+			} else {
+				this.statusLabel.setText(null);
+			}
+		} catch (PatternSyntaxException e) {
+			this.statusLabel.setText(e.getDescription());
+		}
+	}
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() instanceof JCheckBox) {
-            this.updateFind();
-        }
-    }
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() instanceof JCheckBox) {
+			this.updateFind();
+		}
+	}
 
-    @Override
-    public void escapePressed() {
-        this.setVisible(false);
-    }
+	@Override
+	public void escapePressed() {
+		this.setVisible(false);
+	}
 }
--- src/main/java/org/quiltmc/syntaxpain/SyntaxpainConfiguration.java
+++ src/main/java/org/quiltmc/syntaxpain/SyntaxpainConfiguration.java
@@ -7,142 +7,142 @@ import java.util.function.Function;
 
 @SuppressWarnings("unused")
 public class SyntaxpainConfiguration {
-    private static Color highlightColor = new Color(0x3333EE);
-    private static Color stringColor = new Color(0xCC6600);
-    private static Color numberColor = new Color(0x999933);
-    private static Color operatorColor = new Color(0x000000);
-    private static Color delimiterColor = new Color(0x000000);
-    private static Color typeColor = new Color(0x000000);
-    private static Color identifierColor = new Color(0x000000);
-    private static Color commentColour = new Color(0x339933);
-    private static Color textColor = new Color(0x000000);
-    private static Color regexColor = new Color(0xcc6600);
+	private static Color highlightColor = new Color(0x3333EE);
+	private static Color stringColor = new Color(0xCC6600);
+	private static Color numberColor = new Color(0x999933);
+	private static Color operatorColor = new Color(0x000000);
+	private static Color delimiterColor = new Color(0x000000);
+	private static Color typeColor = new Color(0x000000);
+	private static Color identifierColor = new Color(0x000000);
+	private static Color commentColour = new Color(0x339933);
+	private static Color textColor = new Color(0x000000);
+	private static Color regexColor = new Color(0xcc6600);
 
-    private static Color lineRulerPrimaryColor = new Color(0x333300);
-    private static Color lineRulerSecondaryColor = new Color(0xEEEEFF);
-    private static Color lineRulerSelectionColor = new Color(0xCCCCEE);
+	private static Color lineRulerPrimaryColor = new Color(0x333300);
+	private static Color lineRulerSecondaryColor = new Color(0xEEEEFF);
+	private static Color lineRulerSelectionColor = new Color(0xCCCCEE);
 
-    private static Font editorFont = Font.decode(Font.DIALOG);
-    private static Function<JTextComponent, QuickFindDialog> quickFindDialogFactory = QuickFindDialog::new;
+	private static Font editorFont = Font.decode(Font.DIALOG);
+	private static Function<JTextComponent, QuickFindDialog> quickFindDialogFactory = QuickFindDialog::new;
 
-    public static Color getHighlightColor() {
-        return highlightColor;
-    }
+	public static Color getHighlightColor() {
+		return highlightColor;
+	}
 
-    public static void setHighlightColor(Color highlightColor) {
-        SyntaxpainConfiguration.highlightColor = highlightColor;
-    }
+	public static void setHighlightColor(Color highlightColor) {
+		SyntaxpainConfiguration.highlightColor = highlightColor;
+	}
 
-    public static Color getStringColor() {
-        return stringColor;
-    }
+	public static Color getStringColor() {
+		return stringColor;
+	}
 
-    public static void setStringColor(Color stringColor) {
-        SyntaxpainConfiguration.stringColor = stringColor;
-    }
+	public static void setStringColor(Color stringColor) {
+		SyntaxpainConfiguration.stringColor = stringColor;
+	}
 
-    public static Color getNumberColor() {
-        return numberColor;
-    }
+	public static Color getNumberColor() {
+		return numberColor;
+	}
 
-    public static void setNumberColor(Color numberColor) {
-        SyntaxpainConfiguration.numberColor = numberColor;
-    }
+	public static void setNumberColor(Color numberColor) {
+		SyntaxpainConfiguration.numberColor = numberColor;
+	}
 
-    public static Color getOperatorColor() {
-        return operatorColor;
-    }
+	public static Color getOperatorColor() {
+		return operatorColor;
+	}
 
-    public static void setOperatorColor(Color operatorColor) {
-        SyntaxpainConfiguration.operatorColor = operatorColor;
-    }
+	public static void setOperatorColor(Color operatorColor) {
+		SyntaxpainConfiguration.operatorColor = operatorColor;
+	}
 
-    public static Color getDelimiterColor() {
-        return delimiterColor;
-    }
+	public static Color getDelimiterColor() {
+		return delimiterColor;
+	}
 
-    public static void setDelimiterColor(Color delimiterColor) {
-        SyntaxpainConfiguration.delimiterColor = delimiterColor;
-    }
+	public static void setDelimiterColor(Color delimiterColor) {
+		SyntaxpainConfiguration.delimiterColor = delimiterColor;
+	}
 
-    public static Color getTypeColor() {
-        return typeColor;
-    }
+	public static Color getTypeColor() {
+		return typeColor;
+	}
 
-    public static void setTypeColor(Color typeColor) {
-        SyntaxpainConfiguration.typeColor = typeColor;
-    }
+	public static void setTypeColor(Color typeColor) {
+		SyntaxpainConfiguration.typeColor = typeColor;
+	}
 
-    public static Color getIdentifierColor() {
-        return identifierColor;
-    }
+	public static Color getIdentifierColor() {
+		return identifierColor;
+	}
 
-    public static void setIdentifierColor(Color identifierColor) {
-        SyntaxpainConfiguration.identifierColor = identifierColor;
-    }
+	public static void setIdentifierColor(Color identifierColor) {
+		SyntaxpainConfiguration.identifierColor = identifierColor;
+	}
 
-    public static Color getCommentColour() {
-        return commentColour;
-    }
+	public static Color getCommentColour() {
+		return commentColour;
+	}
 
-    public static void setCommentColour(Color commentColour) {
-        SyntaxpainConfiguration.commentColour = commentColour;
-    }
+	public static void setCommentColour(Color commentColour) {
+		SyntaxpainConfiguration.commentColour = commentColour;
+	}
 
-    public static Color getTextColor() {
-        return textColor;
-    }
+	public static Color getTextColor() {
+		return textColor;
+	}
 
-    public static void setTextColor(Color textColor) {
-        SyntaxpainConfiguration.textColor = textColor;
-    }
+	public static void setTextColor(Color textColor) {
+		SyntaxpainConfiguration.textColor = textColor;
+	}
 
-    public static Color getRegexColor() {
-        return regexColor;
-    }
+	public static Color getRegexColor() {
+		return regexColor;
+	}
 
-    public static void setRegexColor(Color regexColor) {
-        SyntaxpainConfiguration.regexColor = regexColor;
-    }
+	public static void setRegexColor(Color regexColor) {
+		SyntaxpainConfiguration.regexColor = regexColor;
+	}
 
-    public static Color getLineRulerPrimaryColor() {
-        return lineRulerPrimaryColor;
-    }
+	public static Color getLineRulerPrimaryColor() {
+		return lineRulerPrimaryColor;
+	}
 
-    public static void setLineRulerPrimaryColor(Color lineRulerPrimaryColor) {
-        SyntaxpainConfiguration.lineRulerPrimaryColor = lineRulerPrimaryColor;
-    }
+	public static void setLineRulerPrimaryColor(Color lineRulerPrimaryColor) {
+		SyntaxpainConfiguration.lineRulerPrimaryColor = lineRulerPrimaryColor;
+	}
 
-    public static Color getLineRulerSecondaryColor() {
-        return lineRulerSecondaryColor;
-    }
+	public static Color getLineRulerSecondaryColor() {
+		return lineRulerSecondaryColor;
+	}
 
-    public static void setLineRulerSecondaryColor(Color lineRulerSecondaryColor) {
-        SyntaxpainConfiguration.lineRulerSecondaryColor = lineRulerSecondaryColor;
-    }
+	public static void setLineRulerSecondaryColor(Color lineRulerSecondaryColor) {
+		SyntaxpainConfiguration.lineRulerSecondaryColor = lineRulerSecondaryColor;
+	}
 
-    public static Color getLineRulerSelectionColor() {
-        return lineRulerSelectionColor;
-    }
+	public static Color getLineRulerSelectionColor() {
+		return lineRulerSelectionColor;
+	}
 
-    public static void setLineRulerSelectionColor(Color lineRulerSelectionColor) {
-        SyntaxpainConfiguration.lineRulerSelectionColor = lineRulerSelectionColor;
-    }
+	public static void setLineRulerSelectionColor(Color lineRulerSelectionColor) {
+		SyntaxpainConfiguration.lineRulerSelectionColor = lineRulerSelectionColor;
+	}
 
-    public static Font getEditorFont() {
-        return editorFont;
-    }
+	public static Font getEditorFont() {
+		return editorFont;
+	}
 
-    public static void setEditorFont(Font font) {
-        editorFont = font;
-        JavaSyntaxKit.setFont(font);
-    }
+	public static void setEditorFont(Font font) {
+		editorFont = font;
+		JavaSyntaxKit.setFont(font);
+	}
 
-    public static QuickFindDialog getQuickFindDialog(JTextComponent component) {
-        return quickFindDialogFactory.apply(component);
-    }
+	public static QuickFindDialog getQuickFindDialog(JTextComponent component) {
+		return quickFindDialogFactory.apply(component);
+	}
 
-    public static void setQuickFindDialogFactory(Function<JTextComponent, QuickFindDialog> dialogFactory) {
-        quickFindDialogFactory = dialogFactory;
-    }
+	public static void setQuickFindDialogFactory(Function<JTextComponent, QuickFindDialog> dialogFactory) {
+		quickFindDialogFactory = dialogFactory;
+	}
 }
