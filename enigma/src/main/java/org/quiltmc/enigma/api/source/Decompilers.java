6af226cc4bab5a6f8e44c0235a86b2c2097ec71a
ix0rai <ix0rai64@gmail.com>
2023-12-17 22:48:11 -0600

    finish

--- enigma/src/main/java/org/quiltmc/enigma/api/source/Decompilers.java
+++ enigma/src/main/java/org/quiltmc/enigma/api/source/Decompilers.java
@@ -1,13 +1,54 @@
 package org.quiltmc.enigma.api.source;
 
+import org.quiltmc.enigma.api.class_provider.ClassProvider;
 import org.quiltmc.enigma.impl.source.bytecode.BytecodeDecompiler;
 import org.quiltmc.enigma.impl.source.cfr.CfrDecompiler;
 import org.quiltmc.enigma.impl.source.procyon.ProcyonDecompiler;
 import org.quiltmc.enigma.impl.source.vineflower.VineflowerDecompiler;
 
 public class Decompilers {
-	public static final DecompilerService VINEFLOWER = VineflowerDecompiler::new;
-	public static final DecompilerService PROCYON = ProcyonDecompiler::new;
-	public static final DecompilerService CFR = CfrDecompiler::new;
-	public static final DecompilerService BYTECODE = BytecodeDecompiler::new;
+	public static final DecompilerService VINEFLOWER = new DecompilerService() {
+		@Override
+		public Decompiler create(ClassProvider classProvider, SourceSettings settings) {
+			return new VineflowerDecompiler(classProvider, settings);
+		}
+
+		@Override
+		public String getId() {
+			return "enigma:vineflower";
+		}
+	};
+	public static final DecompilerService PROCYON = new DecompilerService() {
+		@Override
+		public Decompiler create(ClassProvider classProvider, SourceSettings settings) {
+			return new ProcyonDecompiler(classProvider, settings);
+		}
+
+		@Override
+		public String getId() {
+			return "enigma:procyon";
+		}
+	};
+	public static final DecompilerService CFR = new DecompilerService() {
+		@Override
+		public Decompiler create(ClassProvider classProvider, SourceSettings settings) {
+			return new CfrDecompiler(classProvider, settings);
+		}
+
+		@Override
+		public String getId() {
+			return null;
+		}
+	};
+	public static final DecompilerService BYTECODE = new DecompilerService() {
+		@Override
+		public Decompiler create(ClassProvider classProvider, SourceSettings settings) {
+			return new BytecodeDecompiler(classProvider, settings);
+		}
+
+		@Override
+		public String getId() {
+			return "enigma:bytecode";
+		}
+	};
 }
