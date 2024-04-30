5d206b685475fe7f42c056a0f38cfe4b34e1aa3d
ix0rai <ix0rai64@gmail.com>
2024-04-30 11:59:03 -0500

    fix up tests

--- enigma/src/test/java/org/quiltmc/enigma/input/Keep.java
+++ enigma/src/test/java/org/quiltmc/enigma/input/Keep.java
@@ -1,20 +1,7 @@
 package org.quiltmc.enigma.input;
 
-import java.util.function.Consumer;
-
 public class Keep {
 	public static void main(String... args) {
 		System.out.println("Keep me!");
-	}
-
-	public static void main(String g) {
-		gaming((s) -> {
-			System.out.println(s);
-			gaming((q) -> System.out.println(q));
-		});
-	}
-
-	public static void gaming(Consumer<String> runnable) {
-		runnable.accept("Gaming!");
 	}
 }
