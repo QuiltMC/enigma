a0c603fdd56b725d0ff49b5d2f929ebddaad527d
ix0rai <ix0rai64@gmail.com>
2024-10-15 21:50:06 -0500

    add a test to run the help command

---
+++ enigma-cli/src/test/java/org/quiltmc/enigma/command/HelpCommandTest.java
@@ -0,0 +1,11 @@
+package org.quiltmc.enigma.command;
+
+import org.junit.jupiter.api.Test;
+
+public class HelpCommandTest {
+	@Test
+	void test() throws Exception {
+		// for manually verifying output
+		new HelpCommand().run();
+	}
+}
