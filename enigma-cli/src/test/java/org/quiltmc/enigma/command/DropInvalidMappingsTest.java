eddc32434d3dcfeaf49225012ff2200ea6e02857
ix0rai <ix0rai64@gmail.com>
2024-12-04 19:46:12 -0600

    logic cleanup and new test

--- enigma-cli/src/test/java/org/quiltmc/enigma/command/DropInvalidMappingsTest.java
+++ enigma-cli/src/test/java/org/quiltmc/enigma/command/DropInvalidMappingsTest.java
@@ -8,19 +8,23 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class DropInvalidMappingsTest extends CommandTest {
-	private static final Path JAR = TestUtil.obfJar("lone_class");
+	private static final Path LONE_JAR = TestUtil.obfJar("lone_class");
+	private static final Path INNER_JAR = TestUtil.obfJar("inner_classes");
 	private static final Path INPUT_DIR = getResource("/drop_invalid_mappings/input/");
 	private static final Path EXPECTED_DIR = getResource("/drop_invalid_mappings/expected/");
+
 	private static final Path INVALID_MAPPINGS_INPUT = INPUT_DIR.resolve("InvalidMappings.mapping");
 	private static final Path INVALID_MAPPINGS_EXPECTED = EXPECTED_DIR.resolve("InvalidMappings.mapping");
 	private static final Path EMPTY_MAPPINGS_INPUT = INPUT_DIR.resolve("EmptyMappings.mapping");
 	private static final Path EMPTY_MAPPINGS_EXPECTED = EXPECTED_DIR.resolve("EmptyMappings.mapping");
+	private static final Path MAPPING_SAVE_INPUT = INPUT_DIR.resolve("MappingSave.mapping");
+	private static final Path MAPPING_SAVE_EXPECTED = EXPECTED_DIR.resolve("MappingSave.mapping");
 
 	@Test
 	public void testInvalidMappings() throws Exception {
 		Path resultFile = Files.createTempFile("invalidMappingsResult", ".mapping");
 
-		DropInvalidMappingsCommand.run(JAR, INVALID_MAPPINGS_INPUT, resultFile);
+		DropInvalidMappingsCommand.run(LONE_JAR, INVALID_MAPPINGS_INPUT, resultFile);
 
 		String expectedLines = Files.readString(INVALID_MAPPINGS_EXPECTED);
 		String actualLines = Files.readString(resultFile);
@@ -32,9 +36,21 @@ public class DropInvalidMappingsTest extends CommandTest {
 	public void testEmptyMappings() throws Exception {
 		Path resultFile = Files.createTempFile("emptyMappingsResult", ".mapping");
 
-		DropInvalidMappingsCommand.run(JAR, EMPTY_MAPPINGS_INPUT, resultFile);
+		DropInvalidMappingsCommand.run(LONE_JAR, EMPTY_MAPPINGS_INPUT, resultFile);
 
 		String expectedLines = Files.readString(EMPTY_MAPPINGS_EXPECTED);
+		String actualLines = Files.readString(resultFile);
+
+		Assertions.assertEquals(expectedLines, actualLines);
+	}
+
+	@Test
+	public void testMappingSave() throws Exception {
+		Path resultFile = Files.createTempFile("mappingSaveResult", ".mapping");
+
+		DropInvalidMappingsCommand.run(INNER_JAR, MAPPING_SAVE_INPUT, resultFile);
+
+		String expectedLines = Files.readString(MAPPING_SAVE_EXPECTED);
 		String actualLines = Files.readString(resultFile);
 
 		Assertions.assertEquals(expectedLines, actualLines);
