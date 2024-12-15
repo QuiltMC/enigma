258795abe6a5d6866f652ecc3a5c752c0a1757bd
ix0rai <ix0rai64@gmail.com>
2024-12-14 21:46:09 -0600

    checkstyle

--- enigma/src/test/java/org/quiltmc/enigma/records/TestRecordComponentProposal.java
+++ enigma/src/test/java/org/quiltmc/enigma/records/TestRecordComponentProposal.java
@@ -1,7 +1,6 @@
 package org.quiltmc.enigma.records;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.quiltmc.enigma.TestEntryFactory;
