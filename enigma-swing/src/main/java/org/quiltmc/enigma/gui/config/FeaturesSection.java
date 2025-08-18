c2f3e8917ad4c80266fe04740f9a3582ce3abb5d
ix0rai <ix0rai64@gmail.com>
2025-08-17 20:42:03 -0500

    disable by default

--- enigma-swing/src/main/java/org/quiltmc/enigma/gui/config/FeaturesSection.java
+++ enigma-swing/src/main/java/org/quiltmc/enigma/gui/config/FeaturesSection.java
@@ -11,5 +11,5 @@ public class FeaturesSection extends ReflectiveConfig.Section {
 	@Comment("Enables statistic icons in the class tree. This has a major performance impact on JAR files with lots of classes.")
 	public final TrackedValue<Boolean> enableClassTreeStatIcons = this.value(true);
 	@Comment("Enables auto save functionality, which will automatically save mappings when a change is made.")
-	public final TrackedValue<Boolean> autoSaveMappings = this.value(true);
+	public final TrackedValue<Boolean> autoSaveMappings = this.value(false);
 }
