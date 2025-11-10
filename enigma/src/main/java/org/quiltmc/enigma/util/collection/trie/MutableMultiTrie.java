21f89019ec39d7f402837a02de0c5d2e4525f174
supersaiyansubtlety <supersaiyansubtlety@gmail.com>
2025-11-09 22:01:44 -0800

    promote View

--- enigma/src/main/java/org/quiltmc/enigma/util/collection/trie/MutableMultiTrie.java
+++ enigma/src/main/java/org/quiltmc/enigma/util/collection/trie/MutableMultiTrie.java
@@ -7,8 +7,14 @@ public interface MutableMultiTrie<K, S, V> extends MultiTrie<K, S, V> {
 
 	boolean removeAll(S sequence);
 
+	/**
+	 * @return a live, unmodifiable view of this trie
+	 */
 	MultiTrie<K, S, V> view();
 
+	/**
+	 * @implSpec implementations should not have {@code public} visibility; users should never see node mutation methods
+	 */
 	interface Node<K, S, V> extends MultiTrie.Node<K, V> {
 		void put(S sequence, V value);
 
---
+++ enigma/src/main/java/org/quiltmc/enigma/util/collection/trie/View.java
@@ -0,0 +1,25 @@
+package org.quiltmc.enigma.util.collection.trie;
+
+import javax.annotation.Nonnull;
+
+/**
+ * An unmodifiable view of a {@link MutableMultiTrie}, for use in {@link MutableMultiTrie#view()} implementations.
+ */
+public final class View<K, S, V> implements MultiTrie<K, S, V> {
+	private final MutableMultiTrie<K, S, V> viewed;
+
+	public View(MutableMultiTrie<K, S, V> viewed) {
+		this.viewed = viewed;
+	}
+
+	@Override
+	public Node<K, V> getRoot() {
+		return this.viewed.getRoot();
+	}
+
+	@Nonnull
+	@Override
+	public Node<K, V> get(S prefix) {
+		return this.viewed.get(prefix);
+	}
+}
