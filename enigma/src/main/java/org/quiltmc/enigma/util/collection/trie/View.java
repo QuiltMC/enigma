504412fbdf468f6c34a4780629ca65e3e5c49b00
supersaiyansubtlety <supersaiyansubtlety@gmail.com>
2025-11-09 22:09:35 -0800

    require non-null field assignments

--- enigma/src/main/java/org/quiltmc/enigma/util/collection/trie/View.java
+++ enigma/src/main/java/org/quiltmc/enigma/util/collection/trie/View.java
@@ -1,5 +1,7 @@
 package org.quiltmc.enigma.util.collection.trie;
 
+import org.quiltmc.enigma.util.Utils;
+
 import javax.annotation.Nonnull;
 
 /**
@@ -9,7 +11,7 @@ public final class View<K, S, V> implements MultiTrie<K, S, V> {
 	private final MutableMultiTrie<K, S, V> viewed;
 
 	public View(MutableMultiTrie<K, S, V> viewed) {
-		this.viewed = viewed;
+		this.viewed = Utils.requireNonNull(viewed, "viewed");
 	}
 
 	@Override
