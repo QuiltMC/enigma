828904bd073d737bf762652f177f778204edff4f
supersaiyansubtlety <supersaiyansubtlety@gmail.com>
2025-11-17 16:31:04 -0800

    make :enigma-cli:test depend on test input obf tasks make SearchMappingsTest use its own, islolated jar

---
+++ enigma/src/test/java/org/quiltmc/enigma/input/search_mappings/Obf_a.java
@@ -0,0 +1,13 @@
+package org.quiltmc.enigma.input.search_mappings;
+
+public class Obf_a {
+	private final String name;
+
+	public Obf_a(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+}
