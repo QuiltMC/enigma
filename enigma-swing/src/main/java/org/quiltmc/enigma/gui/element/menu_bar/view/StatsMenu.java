4e59262edc94b146e736d5c7a14ee77b9dd3cea7
supersaiyansubtlety <supersaiyansubtlety@gmail.com>
2025-11-19 20:00:42 -0800

    make StatsMenu and TypeMenu sub-itmes searchable

--- enigma-swing/src/main/java/org/quiltmc/enigma/gui/element/menu_bar/view/StatsMenu.java
+++ enigma-swing/src/main/java/org/quiltmc/enigma/gui/element/menu_bar/view/StatsMenu.java
@@ -5,10 +5,10 @@ import org.quiltmc.enigma.gui.ConnectionState;
 import org.quiltmc.enigma.gui.Gui;
 import org.quiltmc.enigma.gui.config.Config;
 import org.quiltmc.enigma.gui.element.menu_bar.AbstractSearchableEnigmaMenu;
+import org.quiltmc.enigma.gui.element.menu_bar.SimpleCheckBoxItem;
 import org.quiltmc.enigma.util.I18n;
 
 import javax.swing.JCheckBoxMenuItem;
-import javax.swing.JMenu;
 import javax.swing.SwingUtilities;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,11 +18,10 @@ import static java.util.concurrent.CompletableFuture.runAsync;
 public class StatsMenu extends AbstractSearchableEnigmaMenu {
 	private static final String TRANSLATION_KEY = "menu.view.stat_icons";
 
-	private final JCheckBoxMenuItem enableIcons = new JCheckBoxMenuItem();
-	private final JCheckBoxMenuItem includeSynthetic = new JCheckBoxMenuItem();
-	private final JCheckBoxMenuItem countFallback = new JCheckBoxMenuItem();
-	private final JMenu statTypes = new JMenu();
-	private final Map<StatType, JCheckBoxMenuItem> statTypeItems = new HashMap<>();
+	private final SimpleCheckBoxItem enableIcons = new SimpleCheckBoxItem("menu.view.stat_icons.enable_icons");
+	private final SimpleCheckBoxItem includeSynthetic = new SimpleCheckBoxItem("menu.view.stat_icons.include_synthetic");
+	private final SimpleCheckBoxItem countFallback = new SimpleCheckBoxItem("menu.view.stat_icons.count_fallback");
+	private final TypeMenu typeMenu = new TypeMenu();
 
 	public StatsMenu(Gui gui) {
 		super(gui);
@@ -30,32 +29,21 @@ public class StatsMenu extends AbstractSearchableEnigmaMenu {
 		this.add(this.enableIcons);
 		this.add(this.includeSynthetic);
 		this.add(this.countFallback);
-		this.add(this.statTypes);
+		this.add(this.typeMenu);
 
 		this.enableIcons.addActionListener(e -> this.onEnableIconsClicked());
 		this.includeSynthetic.addActionListener(e -> this.onIncludeSyntheticClicked());
 		this.countFallback.addActionListener(e -> this.onCountFallbackClicked());
-		for (StatType statType : StatType.values()) {
-			JCheckBoxMenuItem checkbox = new JCheckBoxMenuItem(statType.getName());
-			checkbox.addActionListener(event -> this.onCheckboxClicked(statType));
-
-			this.statTypeItems.put(statType, checkbox);
-			this.statTypes.add(checkbox);
-		}
 	}
 
 	@Override
 	public void retranslate() {
 		this.setText(I18n.translate(TRANSLATION_KEY));
 
-		this.enableIcons.setText(I18n.translate("menu.view.stat_icons.enable_icons"));
-		this.includeSynthetic.setText(I18n.translate("menu.view.stat_icons.include_synthetic"));
-		this.countFallback.setText(I18n.translate("menu.view.stat_icons.count_fallback"));
-		this.statTypes.setText(I18n.translate("menu.view.stat_icons.included_types"));
-
-		for (StatType statType : StatType.values()) {
-			this.statTypeItems.get(statType).setText(statType.getName());
-		}
+		this.enableIcons.retranslate();
+		this.includeSynthetic.retranslate();
+		this.countFallback.retranslate();
+		this.typeMenu.retranslate();
 	}
 
 	@Override
@@ -64,10 +52,7 @@ public class StatsMenu extends AbstractSearchableEnigmaMenu {
 		this.includeSynthetic.setSelected(Config.main().stats.shouldIncludeSyntheticParameters.value());
 		this.countFallback.setSelected(Config.main().stats.shouldCountFallbackNames.value());
 
-		for (StatType type : StatType.values()) {
-			JCheckBoxMenuItem checkbox = this.statTypeItems.get(type);
-			checkbox.setSelected(Config.main().stats.includedStatTypes.value().contains(type));
-		}
+		this.typeMenu.updateState(jarOpen, state);
 	}
 
 	private void onEnableIconsClicked() {
@@ -85,18 +70,6 @@ public class StatsMenu extends AbstractSearchableEnigmaMenu {
 		this.updateIconsLater();
 	}
 
-	private void onCheckboxClicked(StatType type) {
-		JCheckBoxMenuItem checkbox = this.statTypeItems.get(type);
-
-		if (checkbox.isSelected() && !Config.stats().includedStatTypes.value().contains(type)) {
-			Config.stats().includedStatTypes.value().add(type);
-		} else {
-			Config.stats().includedStatTypes.value().remove(type);
-		}
-
-		this.updateIconsLater();
-	}
-
 	private void updateIconsLater() {
 		SwingUtilities.invokeLater(() -> runAsync(() -> this.gui.getController().regenerateAndUpdateStatIcons()));
 	}
@@ -104,5 +77,55 @@ public class StatsMenu extends AbstractSearchableEnigmaMenu {
 	@Override
 	public String getAliasesTranslationKeyPrefix() {
 		return TRANSLATION_KEY;
+	}
+
+	private final class TypeMenu extends AbstractSearchableEnigmaMenu {
+		static final String TRANSLATION_KEY = "menu.view.stat_icons.included_types";
+
+		private final Map<StatType, SimpleCheckBoxItem> items = new HashMap<>();
+
+		TypeMenu() {
+			super(StatsMenu.this.gui);
+
+			for (StatType statType : StatType.values()) {
+				SimpleCheckBoxItem checkbox = new SimpleCheckBoxItem(statType.getTranslationKey());
+				checkbox.addActionListener(event -> this.onTypeClicked(statType));
+
+				this.items.put(statType, checkbox);
+				this.add(checkbox);
+			}
+		}
+
+		@Override
+		public String getAliasesTranslationKeyPrefix() {
+			return TRANSLATION_KEY;
+		}
+
+		@Override
+		public void retranslate() {
+			this.setText(I18n.translate(TRANSLATION_KEY));
+
+			this.items.values().forEach(SimpleCheckBoxItem::retranslate);
+		}
+
+		@Override
+		public void updateState(boolean jarOpen, ConnectionState state) {
+			for (StatType type : StatType.values()) {
+				JCheckBoxMenuItem checkbox = this.items.get(type);
+				checkbox.setSelected(Config.main().stats.includedStatTypes.value().contains(type));
+			}
+		}
+
+		void onTypeClicked(StatType type) {
+			JCheckBoxMenuItem checkbox = this.items.get(type);
+
+			if (checkbox.isSelected() && !Config.stats().includedStatTypes.value().contains(type)) {
+				Config.stats().includedStatTypes.value().add(type);
+			} else {
+				Config.stats().includedStatTypes.value().remove(type);
+			}
+
+			StatsMenu.this.updateIconsLater();
+		}
 	}
 }
