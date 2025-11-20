aa40750a96b2ea6b052a10f306a0d7eb37bd6591
supersaiyansubtlety <supersaiyansubtlety@gmail.com>
2025-11-19 19:33:02 -0800

    make NotificationsMenu level items searchable

---
+++ enigma-swing/src/main/java/org/quiltmc/enigma/gui/element/menu_bar/SimpleRadioItem.java
@@ -0,0 +1,33 @@
+package org.quiltmc.enigma.gui.element.menu_bar;
+
+import org.quiltmc.enigma.util.I18n;
+
+import javax.swing.JRadioButtonMenuItem;
+
+public class SimpleRadioItem extends JRadioButtonMenuItem implements SearchableElement, Retranslatable {
+	private final String translationKey;
+
+	public SimpleRadioItem(String translationKey) {
+		this.translationKey = translationKey;
+	}
+
+	@Override
+	public String getSearchName() {
+		return this.getText();
+	}
+
+	@Override
+	public String getAliasesTranslationKeyPrefix() {
+		return this.translationKey;
+	}
+
+	@Override
+	public void onSearchClicked() {
+		this.doClick();
+	}
+
+	@Override
+	public void retranslate() {
+		this.setText(I18n.translate(this.translationKey));
+	}
+}
