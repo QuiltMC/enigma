db3ae28571f31a1d846a9f72a1543ff0a5f535f8
supersaiyansubtlety <supersaiyansubtlety@gmail.com>
2025-11-19 18:28:23 -0800

    make EntryTooltipsMenu's sub-items searchable

--- enigma-swing/src/main/java/org/quiltmc/enigma/gui/element/menu_bar/view/EntryTooltipsMenu.java
+++ enigma-swing/src/main/java/org/quiltmc/enigma/gui/element/menu_bar/view/EntryTooltipsMenu.java
@@ -3,20 +3,22 @@ package org.quiltmc.enigma.gui.element.menu_bar.view;
 import org.quiltmc.enigma.gui.Gui;
 import org.quiltmc.enigma.gui.config.Config;
 import org.quiltmc.enigma.gui.element.menu_bar.AbstractSearchableEnigmaMenu;
+import org.quiltmc.enigma.gui.element.menu_bar.SimpleCheckBoxItem;
 import org.quiltmc.enigma.util.I18n;
 
-import javax.swing.JCheckBoxMenuItem;
-
-import static org.quiltmc.enigma.gui.util.GuiUtil.createSyncedMenuCheckBox;
+import static org.quiltmc.enigma.gui.util.GuiUtil.syncStateWithConfig;
 
 public class EntryTooltipsMenu extends AbstractSearchableEnigmaMenu {
 	private static final String TRANSLATION_KEY = "menu.view.entry_tooltips";
 
-	private final JCheckBoxMenuItem enable = createSyncedMenuCheckBox(Config.editor().entryTooltips.enable);
-	private final JCheckBoxMenuItem interactable = createSyncedMenuCheckBox(Config.editor().entryTooltips.interactable);
+	private final SimpleCheckBoxItem enable = new SimpleCheckBoxItem("menu.view.entry_tooltips.enable");
+	private final SimpleCheckBoxItem interactable = new SimpleCheckBoxItem("menu.view.entry_tooltips.interactable");
 
 	protected EntryTooltipsMenu(Gui gui) {
 		super(gui);
+
+		syncStateWithConfig(this.enable, Config.editor().entryTooltips.enable);
+		syncStateWithConfig(this.interactable, Config.editor().entryTooltips.interactable);
 
 		this.add(this.enable);
 		this.add(this.interactable);
@@ -27,8 +29,8 @@ public class EntryTooltipsMenu extends AbstractSearchableEnigmaMenu {
 	@Override
 	public void retranslate() {
 		this.setText(I18n.translate(TRANSLATION_KEY));
-		this.enable.setText(I18n.translate("menu.view.entry_tooltips.enable"));
-		this.interactable.setText(I18n.translate("menu.view.entry_tooltips.interactable"));
+		this.enable.retranslate();
+		this.interactable.retranslate();
 	}
 
 	@Override
