177b6421d81dfa7b59ac33025a4dcb5bf8c38d25
supersaiyansubtlety <supersaiyansubtlety@gmail.com>
2025-11-24 14:12:25 -0800

    extract ConstrainedGrid from FlexGridLayout add Relative Constraints

---
+++ enigma-swing/src/main/java/org/quiltmc/enigma/gui/util/ConstrainedGrid.java
@@ -0,0 +1,87 @@
+package org.quiltmc.enigma.gui.util;
+
+import org.quiltmc.enigma.gui.util.FlexGridLayout.Constrained;
+
+import java.awt.Component;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.stream.Stream;
+
+/**
+ * A map of cartesian coordinates to {@link Constrained} values.<br>
+ * Only designed for use with {@link FlexGridLayout}.
+ *
+ * <p> Multiple values can be associated with the same coordinates,
+ * but a value may only be associated with one coordinate pair at a time.
+ */
+class ConstrainedGrid {
+	private static Set<Component> createValueSet(Integer ignored) {
+		return new HashSet<>();
+	}
+
+	private final Map<Integer, Map<Integer, Map<Component, Constrained>>> grid = new HashMap<>();
+	private final Map<Component, Position> valuePositions = new HashMap<>();
+	private final SortedMap<Integer, Set<Component>> valuesByMaxX = new TreeMap<>();
+	private final SortedMap<Integer, Set<Component>> valuesByMaxY = new TreeMap<>();
+
+	public void put(int x, int y, Constrained value) {
+		final Component component = value.component();
+		final Position oldPos = this.valuePositions.replace(component, new Position(x, y));
+		if (oldPos != null) {
+			final Map<Integer, Map<Component, Constrained>> column = this.grid.get(oldPos.x);
+			final Constrained oldValue = column.get(oldPos.y).get(component);
+			column.get(oldPos.y).remove(component, value);
+			this.valuesByMaxX.get(oldPos.x + oldValue.getXExcess()).remove(component);
+			this.valuesByMaxY.get(oldPos.y + oldValue.getYExcess()).remove(component);
+		}
+
+		this.grid
+				.computeIfAbsent(x, ignored -> new HashMap<>())
+				.computeIfAbsent(y, ignored -> new HashMap<>(1))
+				.put(component, value);
+		this.valuesByMaxX.computeIfAbsent(x + value.getXExcess(), ConstrainedGrid::createValueSet).add(component);
+		this.valuesByMaxY.computeIfAbsent(y + value.getYExcess(), ConstrainedGrid::createValueSet).add(component);
+	}
+
+	public Stream<Constrained> get(int x, int y) {
+		return this.grid.getOrDefault(x, Map.of())
+			.getOrDefault(y, Map.of())
+			.values()
+			.stream();
+	}
+
+	public boolean remove(Component value) {
+		final Position pos = this.valuePositions.remove(value);
+		if (pos != null) {
+			final Constrained removed = this.grid.get(pos.x).get(pos.y).remove(value);
+			this.valuesByMaxX.get(pos.x + removed.getXExcess()).remove(value);
+			this.valuesByMaxY.get(pos.y + removed.getYExcess()).remove(value);
+
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public int getMaxXOrThrow() {
+		return this.valuesByMaxX.lastKey();
+	}
+
+	public int getMaxYOrThrow() {
+		return this.valuesByMaxY.lastKey();
+	}
+
+	public int getSize() {
+		return this.valuesByMaxX.size();
+	}
+
+	public boolean isEmpty() {
+		return this.valuesByMaxX.isEmpty();
+	}
+
+	private record Position(int x, int y) { }
+}
