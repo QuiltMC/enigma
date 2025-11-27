c0aeed032562bcaa693f59d4bd6d601e1ad171f5
supersaiyansubtlety <supersaiyansubtlety@gmail.com>
2025-11-27 10:05:09 -0800

    add selection border to PlaceheldTextField scale SearchMenusMenu.field's font select SearchMenusMenu.field when focusing it

--- enigma-swing/src/main/java/org/quiltmc/enigma/gui/element/PlaceheldTextField.java
+++ enigma-swing/src/main/java/org/quiltmc/enigma/gui/element/PlaceheldTextField.java
@@ -2,14 +2,20 @@ package org.quiltmc.enigma.gui.element;
 
 import org.jspecify.annotations.Nullable;
 import org.quiltmc.enigma.gui.util.GuiUtil;
+import org.quiltmc.enigma.gui.util.ScaleUtil;
 import org.quiltmc.enigma.util.Utils;
 
 import javax.swing.JTextField;
 import javax.swing.MenuElement;
 import javax.swing.MenuSelectionManager;
+import javax.swing.UIManager;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.MatteBorder;
 import javax.swing.text.Document;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
@@ -17,15 +23,29 @@ import java.awt.Insets;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 
+import static javax.swing.BorderFactory.createCompoundBorder;
+import static javax.swing.BorderFactory.createEmptyBorder;
+
 /**
  * A text field that displays placeholder text when it's empty.
  */
 public class PlaceheldTextField extends JTextField implements MenuElement {
+	private static final int DEFAULT_SELECTION_BORDER_LEFT = ScaleUtil.scale(3);
+	private static final int DEFAULT_SELECTION_BORDER_RIGHT = DEFAULT_SELECTION_BORDER_LEFT;
+	private static final int DEFAULT_SELECTION_BORDER_TOP = ScaleUtil.scale(1);
+	private static final int DEFAULT_SELECTION_BORDER_BOTTOM = DEFAULT_SELECTION_BORDER_TOP;
+
 	@Nullable
 	private Placeholder placeholder;
 
 	@Nullable
 	private Color placeholderColor;
+
+	private CompoundBorder defaultBorder;
+
+	private CompoundBorder selectionBorder;
+
+	private boolean selectionIncluded;
 
 	/**
 	 * Constructs a new field with the default {@link Document},  {@code 0} columns, and no initial text or placeholder.
@@ -61,6 +81,26 @@ public class PlaceheldTextField extends JTextField implements MenuElement {
 		super(doc, text, columns);
 
 		this.placeholder = new Placeholder(placeholder);
+
+		final Border originalBorder = this.getBorder();
+		this.selectionBorder = createCompoundBorder(
+			new MatteBorder(
+				DEFAULT_SELECTION_BORDER_TOP, DEFAULT_SELECTION_BORDER_LEFT,
+				DEFAULT_SELECTION_BORDER_BOTTOM, DEFAULT_SELECTION_BORDER_RIGHT,
+				UIManager.getColor("MenuItem.selectionBackground")
+			),
+			originalBorder
+		);
+
+		this.defaultBorder = createCompoundBorder(
+			createEmptyBorder(
+				DEFAULT_SELECTION_BORDER_TOP, DEFAULT_SELECTION_BORDER_LEFT,
+				DEFAULT_SELECTION_BORDER_BOTTOM, DEFAULT_SELECTION_BORDER_RIGHT
+			),
+			originalBorder
+		);
+
+		super.setBorder(this.defaultBorder);
 	}
 
 	@Override
@@ -74,6 +114,66 @@ public class PlaceheldTextField extends JTextField implements MenuElement {
 		}
 
 		return size;
+	}
+
+	@Override
+	public void setBorder(Border border) {
+		if (this.selectionBorder == null) {
+			// JTextField sets border in constructor before selectionBorder is initialized
+			super.setBorder(border);
+		} else {
+			this.defaultBorder = this.createDefaultBorder(border);
+			this.selectionBorder = createCompoundBorder(this.selectionBorder.getOutsideBorder(), border);
+			super.setBorder(this.defaultBorder);
+		}
+	}
+
+	private CompoundBorder createDefaultBorder(Border border) {
+		final Border selectionOuter = this.selectionBorder.getOutsideBorder();
+		if (selectionOuter == null) {
+			return createCompoundBorder(null, border);
+		} else {
+			final Insets selectionInsets = selectionOuter.getBorderInsets(this);
+			return createCompoundBorder(
+				createEmptyBorder(selectionInsets.top, selectionInsets.left, selectionInsets.bottom, selectionInsets.right),
+				border
+			);
+		}
+	}
+
+	public void setSelectionBorder(Border border) {
+		final Border insideBorder = this.defaultBorder.getInsideBorder();
+		this.selectionBorder = createCompoundBorder(border, insideBorder);
+		this.defaultBorder = this.createDefaultBorder(insideBorder);
+		super.setBorder(this.defaultBorder);
+	}
+
+	@Override
+	protected void paintBorder(Graphics graphics) {
+		if (this.selectionIncluded) {
+			final int leftInset;
+			final int topInset;
+			{
+				final Container parent = this.getParent();
+
+				if (parent == null) {
+					leftInset = 0;
+					topInset = 0;
+				} else {
+					final Insets insets = parent.getInsets();
+					leftInset = insets.left;
+					topInset = insets.top;
+				}
+			}
+
+			this.selectionBorder.paintBorder(
+					this, graphics,
+					this.getX() - leftInset, this.getY() - topInset,
+					this.getWidth(), this.getHeight()
+			);
+		} else {
+			super.paintBorder(graphics);
+		}
 	}
 
 	@Override
@@ -115,7 +215,13 @@ public class PlaceheldTextField extends JTextField implements MenuElement {
 	public void processKeyEvent(KeyEvent event, MenuElement[] path, MenuSelectionManager manager) { }
 
 	@Override
-	public void menuSelectionChanged(boolean isIncluded) { }
+	public void menuSelectionChanged(boolean isIncluded) {
+		if (this.selectionIncluded != isIncluded) {
+			this.selectionIncluded = isIncluded;
+			// update border
+			this.repaint();
+		}
+	}
 
 	@Override
 	public MenuElement[] getSubElements() {
