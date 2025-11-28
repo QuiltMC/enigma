325e5ff8273d9e487cfe9fe8cd36b1b9e80498ff
supersaiyansubtlety <supersaiyansubtlety@gmail.com>
2025-11-27 21:03:26 -0800

    use Visualizer interface instead of making visualizers extend JFrame add FlexGridGridVisualiser

--- enigma-swing/src/guiVisualization/java/org/quilt/internal/gui/visualization/FlexGridColumnVisualiser.java
+++ enigma-swing/src/guiVisualization/java/org/quilt/internal/gui/visualization/FlexGridColumnVisualiser.java
@@ -6,19 +6,21 @@ import org.quiltmc.enigma.gui.util.FlexGridLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class FlexGridColumnVisualiser extends JFrame {
-	public static final String TITLE = "Flex Grid Column";
+public class FlexGridColumnVisualiser implements Visualizer {
+	@Override
+	public String getTitle() {
+		return "Flex Grid Column";
+	}
 
-	public FlexGridColumnVisualiser() {
-		super(TITLE);
-
-		this.setLayout(new FlexGridLayout());
+	@Override
+	public void visualizeWindow(JFrame window) {
+		window.setLayout(new FlexGridLayout());
 
 		final FlexGridConstraints.Absolute constraints = FlexGridConstraints.createAbsolute();
-		this.add(new JLabel("Label 1"), constraints);
-		this.add(new JLabel("Label 2"), constraints.nextRow());
-		this.add(new JLabel("Label 3"), constraints.nextRow());
+		window.add(new JLabel("Label 1"), constraints);
+		window.add(new JLabel("Label 2"), constraints.nextRow());
+		window.add(new JLabel("Label 3"), constraints.nextRow());
 
-		this.pack();
+		window.pack();
 	}
 }
