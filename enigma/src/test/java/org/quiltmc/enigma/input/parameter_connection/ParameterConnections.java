d432a76ac1d3b2545e43630b8eac17ef3b78132d
supersaiyansubtlety <supersaiyansubtlety@gmail.com>
2025-12-12 14:35:01 -0800

    remove z_ prefix from z_parameters_connection test input package

--- enigma/src/test/java/org/quiltmc/enigma/input/parameter_connection/ParameterConnections.java
+++ enigma/src/test/java/org/quiltmc/enigma/input/parameter_connection/ParameterConnections.java
@@ -1,4 +1,4 @@
-package org.quiltmc.enigma.input.z_parameter_connection;
+package org.quiltmc.enigma.input.parameter_connection;
 
 public class ParameterConnections {
 	static Object toStringOf(String param) {
