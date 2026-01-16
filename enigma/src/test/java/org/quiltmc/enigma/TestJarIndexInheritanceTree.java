ab786153cf671ac997ba99c717dbfc7aa41a525c
supersaiyansubtlety <supersaiyansubtlety@gmail.com>
2026-01-16 10:49:03 -0800

    remove commented code

--- enigma/src/test/java/org/quiltmc/enigma/TestJarIndexInheritanceTree.java
+++ enigma/src/test/java/org/quiltmc/enigma/TestJarIndexInheritanceTree.java
@@ -52,9 +52,6 @@ public class TestJarIndexInheritanceTree {
 
 	@Test
 	public void obfEntries() {
-		// assertThat(this.index.getIndex(EntryIndex.class).getClasses(), Matchers.containsInAnyOrder(
-		// 		TestEntryFactory.newClass("org/quiltmc/enigma/input/Keep"), BASE_CLASS, SUB_CLASS_A, SUB_CLASS_AA, SUB_CLASS_B
-		// ));
 		assertThat(this.index.getIndex(EntryIndex.class).getClasses(), Matchers.containsInAnyOrder(
 				TestEntryFactory.newClass("org/quiltmc/enigma/input/Keep"),
 				BASE_CLASS, SUB_CLASS_A, SUB_CLASS_AA, SUB_CLASS_B,
