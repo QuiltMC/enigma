f6c3a7230349c189ad3120c4a7050230d4c1ac89
PiTheGuy <55959311+PiTheGuy@users.noreply.github.com>
2026-02-05 09:05:24 -0600

    Redesign stats menu (#343)
    * Format type stats as a table
    
    * Implement generate diagram dialog
    
    * Remove unused class
    
    * Update overall percentage
    
    * Implement settings dialog
    
    * Show active settings
    
    * Improve the look of the progress bars
    
    * Match the color of the two progress bars
    
    * Localize
    
    * Minor UI improvements and fixes
    
    * Checkstyle + code cleanup
    
    * Disable cell selection on table
    
    * remove todo
    
    * Make column enum private
    
    * Fix scaling
    
    * Separate progress bar implementations
    
    * Other minor fixes
    
    * Change translation keys in other languages
    
    * Make package filter persist
    
    * Disable resizing

---
+++ enigma-swing/src/main/java/org/quiltmc/enigma/gui/dialog/stats/StatTableModel.java
@@ -0,0 +1,83 @@
+package org.quiltmc.enigma.gui.dialog.stats;
+
+import org.quiltmc.enigma.api.stats.ProjectStatsResult;
+import org.quiltmc.enigma.api.stats.StatType;
+import org.quiltmc.enigma.util.I18n;
+
+import javax.swing.table.AbstractTableModel;
+import java.util.Comparator;
+import java.util.function.BiFunction;
+
+public class StatTableModel extends AbstractTableModel {
+	private final ProjectStatsResult result;
+
+	public StatTableModel(ProjectStatsResult result) {
+		this.result = result;
+	}
+
+	@Override
+	public int getRowCount() {
+		return this.result.getOverall().getTypes().size();
+	}
+
+	@Override
+	public int getColumnCount() {
+		return Column.values().length;
+	}
+
+	@Override
+	public String getColumnName(int column) {
+		return this.getColumn(column).getName();
+	}
+
+	@Override
+	public Class<?> getColumnClass(int columnIndex) {
+		return this.getColumn(columnIndex).getType();
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		return this.getColumn(columnIndex).getValue(this.result, this.getType(rowIndex));
+	}
+
+	private Column getColumn(int columnIndex) {
+		return Column.values()[columnIndex];
+	}
+
+	private StatType getType(int rowIndex) {
+		return this.result.getOverall().getTypes().stream()
+			.sorted(Comparator.comparing(StatType::getName))
+			.skip(rowIndex)
+			.findFirst()
+			.orElseThrow();
+	}
+
+	private enum Column {
+		TYPE("menu.file.stats.column.type", String.class, (result, type) -> type.getName()),
+		MAPPED("menu.file.stats.column.mapped", Integer.class, ProjectStatsResult::getMapped),
+		TOTAL("menu.file.stats.column.total", Integer.class, ProjectStatsResult::getMappable),
+		PROGRESS_BAR("menu.file.stats.column.progress", Double.class, ProjectStatsResult::getPercentage);
+
+		private final String name;
+		private final Class<?> type;
+		private final BiFunction<ProjectStatsResult, StatType, ?> getter;
+
+		<T> Column(String name, Class<T> type, BiFunction<ProjectStatsResult, StatType, T> getter) {
+			this.name = name;
+			this.type = type;
+			this.getter = getter;
+		}
+
+		public String getName() {
+			return I18n.translate(this.name);
+		}
+
+		public Class<?> getType() {
+			return this.type;
+		}
+
+		public Object getValue(ProjectStatsResult result, StatType type) {
+			return this.getter.apply(result, type);
+		}
+	}
+}
